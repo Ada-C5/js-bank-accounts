aa478c8f08a7eff8d466436fa1fbdd17ef2b3bac
Charles Ellis <hamled@hamled.name>
2016-06-13 20:50:26 -0700

    Account list page

--- controllers/accounts_controller.js
+++ controllers/accounts_controller.js
@@ -1,6 +1,20 @@
 var Account = require("../models/account");
 
 var AccountsController = {
+  index: function(req, res, next) {
+    Account.all(function(error, accounts) {
+      if(error) {
+        var err = new Error("Error retrieving account list:\n" + error.message);
+        err.status = 500;
+        next(err);
+      } else {
+        res.render("accounts/index", {
+          accounts: accounts
+        });
+      }
+    });
+  },
+
   show: function(req, res, next) {
     Account.find(req.params.id, function(error, account) {
       if(error) {
--- routes/accounts.js
+++ routes/accounts.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var AccountsController = require('../controllers/accounts_controller.js')
 
+/* GET account listing. */
+router.get('/', AccountsController.index);
+
 /* GET account details. */
 router.get('/:id', AccountsController.show);
 
